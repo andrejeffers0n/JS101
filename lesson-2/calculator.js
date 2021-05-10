167d926cc7ff6a309ab2f769ca018291d2bdf7b0
Andre Jefferson <andrejeffers0n@outlook.com>
2021-05-09 18:48:30 -0700

    initial calculator app

---
+++ lesson-2/calculator.js
@@ -0,0 +1,35 @@
+// ask the user for the first number
+// ask the user for the second number
+// ask the user for an operation to perform
+// perform the operation
+// print the results to the terminal
+
+const readline = require("readline-sync");
+
+console.log("Welcome to Calculator, your favorite calculator!");
+
+console.log(`What's the first number?`);
+let number1 = readline.question();
+
+console.log(`What's the second number?`);
+let number2 = readline.question();
+
+console.log(
+  `What operation would you like to perform?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide`
+);
+let operation = readline.question();
+
+let output;
+if (operation === "1") {
+  output = number1 + number2;
+} else if (operation === "2") {
+  output = number1 - number2;
+} else if (operation === "3") {
+  output = number1 * number2;
+} else if (operation === "4") {
+  output = number1 / number2;
+} else {
+  output = "No operator chosen";
+}
+
+console.log(`The result is: ${output}. Thank you for choosing Calculator!`);
