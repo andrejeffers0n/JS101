e6c2a6f7a8ccef27ff4e36eda8ad2ebafdd10290
Andre Jefferson <andrejeffers0n@outlook.com>
2021-05-13 15:04:28 -0700

    figuring out how to get eslint working

--- lesson-2/calculator.js
+++ lesson-2/calculator.js
@@ -6,30 +6,54 @@
 
 const readline = require("readline-sync");
 
-console.log("Welcome to Calculator, your favorite calculator!");
-
-console.log(`What's the first number?`);
-let number1 = readline.question();
-
-console.log(`What's the second number?`);
-let number2 = readline.question();
-
-console.log(
-  `What operation would you like to perform?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide`
-);
-let operation = readline.question();
-
-let output;
-if (operation === "1") {
-  output = Number(number1) + Number(number2);
-} else if (operation === "2") {
-  output = Number(number1) - Number(number2);
-} else if (operation === "3") {
-  output = Number(number1) * Number(number2);
-} else if (operation === "4") {
-  output = Number(number1) / Number(number2);
-} else {
-  output = "No operator chosen";
+function prompt(msg) {
+  console.log(`=> ${msg}`);
 }
 
-console.log(`The result is: ${output}. Thank you for choosing Calculator!`);
+function invalidNumber(num) {
+  return num.trimStart() === "" || Number.isNaN(Number(num));
+}
+
+prompt("Welcome to Calculator, your favorite calculator!");
+
+prompt(`What's the first number?`);
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Nah, that ain't a number. Try again!");
+  number1 = readline.question();
+}
+
+prompt(`What's the second number?`);
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Nah, that ain't a number. Try again!");
+  number2 = readline.question();
+}
+
+prompt(`So whatchu tryna do?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide`);
+let operation = readline.question();
+
+while (!["1", "2", "3", "4"].includes(operation)) {
+  prompt("You gotta choose 1, 2, 3, or 4, bruh!");
+  operation = readline.question();
+}
+
+let output;
+switch (operation) {
+  case "1":
+    output = Number(number1) + Number(number2);
+    break;
+  case "2":
+    output = Number(number1) - Number(number2);
+    break;
+  case "3":
+    output = Number(number1) * Number(number2);
+    break;
+  case "4":
+    output = Number(number1) / Number(number2);
+    break;
+}
+
+prompt(`The result is: ${output}. Thank you for choosing Calculator!`);
---
+++ lesson-2/pseudocode.js
@@ -0,0 +1,23 @@
+/*
+START
+
+
+SET result
+SET addNumbers(number1, number2)
+  SET  result = number1 + number 2
+  READ result
+
+END
+*/
+
+/*
+START
+
+
+SET words = ['these', 'will', 'be', 'strings']
+SET addEmUp()
+  SET combinedWords = words.join()
+  READ
+
+PRINT combinedWords
+*/
